ca1d9c4986a192947688f7c8916104f59d1ca680
toino <joaoapereira21@hotmail.com>
2023-05-24 16:03:01 +0100

    Initial code to fetch users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,42 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+    import type { PageData } from "./$types";
+    import octicons from "@primer/octicons";
+
+    export let data: PageData;
+
+    export const dataString = JSON.stringify(data, null, 4);
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<main class="container-lg">
+    <div class="Box m-4">
+        <ol>
+            {#each data.users as user, i}
+                <li class="Box-row d-flex flex-row flex-items-center">
+                    <span class="Label mr-3">{i + 1}</span>
+                    <img
+                        src={user.avatarUrl}
+                        alt=""
+                        class="avatar mr-3"
+                        width="32"
+                        height="32"
+                        loading="lazy"
+                    />
+                    <a href={user.url} class="Box-row-link mr-auto">
+                        {user.name}
+                    </a>
+                    <span class="Counter">
+                        {@html octicons["git-commit"].toSVG()}
+                        {user.commits}
+                    </span>
+                </li>
+            {/each}
+        </ol>
+    </div>
+</main>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<pre>{dataString}</pre>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    resolve: {
+        alias: {
+            "node-fetch": "isomorphic-fetch",
+        },
+    },
 });
