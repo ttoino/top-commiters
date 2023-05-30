4d58b401d5ea1e0206bb221c5c9f5a371f02596e
toino <joaoapereira21@hotmail.com>
2023-05-30 01:08:06 +0100

    Improve populate script

--- populate.sh
+++ populate.sh
@@ -2,13 +2,10 @@
 
 read -ra countries < <(jq -r 'keys | @tsv' </app/countries.json)
 
-curl -s "$HOST:$PORT/global/populate?key=$POPULATE_KEY" >/dev/null
-echo "Populated global"
-
 function populate() {
     curl -s "$HOST:$PORT/$1/populate?key=$POPULATE_KEY" >/dev/null
     echo "Populated $1"
 }
 
 export -f populate
-xargs -I {} -P 4 -n 1 bash -c "populate {}" < <(printf "%s\n" "${countries[@]}")
+xargs -I {} -P 16 -n 1 bash -c "populate {}" < <(printf "%s\n" "${countries[@]}")
