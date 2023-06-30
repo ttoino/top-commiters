86fcceec20d084abe7e8217e1b9e24ab0449bfd2
toino <joaoapereira21@hotmail.com>
2023-06-30 16:32:13 +0100

    Add search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,40 @@
 <script lang="ts">
     import countries from "$lib/countries.json";
+
+    let value = "";
+
+    let filteredCountries: typeof countries[keyof typeof countries][] = [];
+
+    $: filteredCountries = 
+        Object.values(countries).filter((country) => {
+            return [country.name, country.flag, country.code, ...country.alias].map(s => s.toLowerCase().includes(value.toLowerCase())).includes(true);
+        });
 </script>
 
 <svelte:head>
     <title>Top commiters</title>
 </svelte:head>
 
+<input type="search" placeholder="Search countries..." name="filter-country" id="filter-country" class="form-control input-block mb-4 p-3" bind:value>
+
 <div class="Box">
-    <ul>
-        {#each Object.values(countries) as country}
-            <li class="Box-row d-flex flex-row flex-items-center position-relative">
-                <a href={`/${country.code}/contribs`} class="Box-row-link mr-auto">
-                    {country.flag} {country.name}
-                </a>
-            </li>
-        {/each}
-    </ul>
+    {#if filteredCountries.length === 0}
+        <div class="blankslate">
+            <h3 class="blankslate-heading">
+                No countries found
+            </h3>
+        </div>
+    {:else}
+        <ul>
+            {#each filteredCountries as country}
+                <li class="Box-row d-flex flex-row flex-items-center position-relative">
+                    <a href={`/${country.code}/contribs`} class="Box-row-link mr-auto">
+                        {country.flag} {country.name}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    {/if}
 </div>
 
 <style>
