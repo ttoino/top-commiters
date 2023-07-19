b74167c46f3d513368da750ca26bb36f0a237882
toino <joaoapereira21@hotmail.com>
2023-07-19 23:58:26 +0100

    Add custom logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import octicons from "@primer/octicons";
     import ThemeButton from "./ThemeButton.svelte";
+    import logo from "$lib/logo.svg?raw";
 
     const githubIcon = octicons["mark-github"].toSVG({height: 24, width: 24});
 </script>
@@ -8,7 +9,7 @@
 <header class="Header position-sticky top-0">
     <h1 class="Header-item h2 mr-auto">
         <a href="/" class="Header-link">
-            Top commiters
+            {@html logo}<span class="ml-2">Top commiters</span>
         </a>
     </h1>
 
@@ -20,3 +21,12 @@
         <a href="https://github.com/ttoino/top-commiters" class="Header-link" title="Github" aria-label="Top committers Github repository">{@html githubIcon}</a>
     </div>
 </header>
+
+<style>
+    h1 :global(svg) {
+        height: 1em;
+        width: 1em;
+        fill: currentColor;
+        vertical-align: middle;
+    }
+</style>
