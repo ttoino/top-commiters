d07ca8b68745ad4d57bcba0fbfb33c972da2f42f
toino <joaoapereira21@hotmail.com>
2023-12-15 10:16:56 +0000

    Upgrade packages

--- src/lib/gh.ts
+++ src/lib/gh.ts
@@ -1,14 +1,18 @@
-import { env } from "$env/dynamic/private";
+import {
+    GH_APP_ID,
+    GH_APP_PRIVATE_KEY,
+    GH_INSTALLATION_ID,
+} from "$env/static/private";
 import { App } from "octokit";
 
 export const getOctokit = async () => {
     const app = new App({
-        appId: env.GH_APP_ID,
-        privateKey: env.GH_APP_PRIVATE_KEY,
+        appId: GH_APP_ID,
+        privateKey: GH_APP_PRIVATE_KEY,
     });
 
     const octokit = await app.getInstallationOctokit(
-        parseInt(env.GH_INSTALLATION_ID),
+        parseInt(GH_INSTALLATION_ID),
     );
 
     return octokit;
